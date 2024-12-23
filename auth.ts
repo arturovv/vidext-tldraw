b5f7f86c6d6e9e19246c7d6604299e36e5f2cf4b
Arturo Varón <arturo.varon@gmail.com>
2024-12-23 13:17:14 +0100

    set up next-auth

---
+++ auth.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import db from "@/db"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    session: {
+        strategy: "jwt",
+    },
+    adapter: DrizzleAdapter(db),
+    providers: [
+        Nodemailer({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+        }),
+    ],
+})
