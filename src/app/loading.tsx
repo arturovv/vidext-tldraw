878cc0052c2095aef8bc03bbe0695477be982af8
Arturo Varón <arturo.varon@gmail.com>
2024-12-25 18:38:18 +0100

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/ui/loading";
+
+export default function LoadingPage() {
+  return (
+    <main className="fixed inset-0 end-10">
+      <Loading />
+    </main>
+  );
+}
