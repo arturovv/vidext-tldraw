55586f49b8fb1eb73c066728764654f11e142f84
Arturo Varón <arturo.varon@gmail.com>
2024-12-26 12:04:35 +0100

    fix: check user in the server action

--- src/lib/ai/actions.ts
+++ src/lib/ai/actions.ts
@@ -1,9 +1,16 @@
 'use server'
+import { auth } from "@/auth";
 import { getModel } from "./ai";
 
 const DESCRIBE_IMAGE_PROMPT = "Describe the provided image. It was drawn by a human using tldraw.";
 
 export const describeImageWithAI = async (image: Blob) => {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return "UNAUTHORIZED"
+  }
+
   const arrayBuffer = await image.arrayBuffer();
 
   const model = getModel()
