495b21ca4627dbf19a235ea77a1ec1b243c036e8
Arturo Varón <arturo.varon@gmail.com>
2024-12-26 12:02:50 +0100

    show image description in a dialog

--- src/components/ai-describe/ai-describe.tsx
+++ src/components/ai-describe/ai-describe.tsx
@@ -11,6 +11,9 @@ import {
 } from "@/components/ui/tooltip"
 import Link from "next/link"
 import { describeImageWithAI } from "@/lib/ai/actions"
+import { useState } from "react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import Loading from "@/components/ui/loading"
 
 interface AiDescribeProps {
   editor: Editor | null
@@ -19,14 +22,24 @@ interface AiDescribeProps {
 }
 
 export default function AiDescribe({ editor, isLoggedIn, isShapeSelected }: AiDescribeProps) {
+  const [openDialog, setOpenDialog] = useState(false)
+  const [status, setStatus] = useState<"loading" | "idle" | "error">("idle")
+  const [description, setDescription] = useState<string | null>(null)
 
   const handleClick = async () => {
     if (!editor) return
-
-    const shapeIds = editor.getSelectedShapeIds()
-    const blob = await exportToBlob({ editor, ids: shapeIds, format: "png" })
-    const description = await describeImageWithAI(blob)
-    alert(description)
+    setStatus("loading")
+    setOpenDialog(true)
+    try {
+      const shapeIds = editor.getSelectedShapeIds()
+      const blob = await exportToBlob({ editor, ids: shapeIds, format: "png" })
+      const description = await describeImageWithAI(blob)
+      setDescription(description)
+      setStatus("idle")
+    } catch (error) {
+      console.error(error)
+      setStatus("error")
+    }
   }
 
   return (
@@ -44,6 +57,27 @@ export default function AiDescribe({ editor, isLoggedIn, isShapeSelected }: AiDe
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
+
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Draw description</DialogTitle>
+          </DialogHeader>
+
+          {status === "loading" && (
+            <Loading text="Generating description..." />
+          )}
+
+          {status === "error" && (
+            <div>There was an error generating the description</div>
+          )}
+
+          {status === "idle" && (
+            <div>{description}</div>
+          )}
+
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
